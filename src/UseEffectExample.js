193ea1b9788d94946448a9fa178484d06755c42a
patidar12345 <yashdeeppatidar29@gmail.com>
2024-11-21 15:20:43 +0530

    Update UseEffectExample.js

--- src/UseEffectExample.js
+++ src/UseEffectExample.js
@@ -1,23 +1,23 @@
-// import {useState,useEffect } from "react";
-// function UseEffectExample(){
-// let [count,setcount]=useState(0)
+import {useState,useEffect } from "react";
+function UseEffectExample(){
+let [count,setcount]=useState(0)
 
 
-// useEffect(()=>{
-//     document.title=`welcome ${count}`
-// },[count]);
+useEffect(()=>{
+    document.title=`welcome ${count}`
+},[count]);
 
-// function ChangeCount(){
-//     count+=1
-//     setcount(count)
-// }
-// return(<div>
-//     <input type="button" value="click" onClick={ChangeCount}/>
-//     <br/>
-//     <h1>useEffect example ${count}</h1>
-// </div>)
-// }
-// export default UseEffectExample;
+function ChangeCount(){
+    count+=1
+    setcount(count)
+}
+return(<div>
+    <input type="button" value="click" onClick={ChangeCount}/>
+    <br/>
+    <h1>useEffect example ${count}</h1>
+</div>)
+}
+export default UseEffectExample;
 
 
 
